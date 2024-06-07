e4f2b506f9521650c9bc5ae81e1eadc33d310507
fahimhossain26 <ismailhossainfahim64@gmail.com>
2024-06-08 02:11:50 +0600

    users collection ccreate

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjcm9nm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,11 +36,31 @@ async function run() {
     await client.connect();
 
     const campCollection = client.db("MCMS").collection("camps");
+    const usersCollecction=client.db("MCMS").collection("users");
 
     //get all camps
     app.get('/camps',async (req,res)=>{
       const result = await campCollection.find().toArray()
       res.send(result)
+    })
+
+    //save a user data in DB 
+    app.put('/user',async (req,res)=>{
+      const user=req.body
+//check if user already exist in Db 
+const isExist= await usersCollecction.findOne({email: user?.email})
+if(isExist) return res.send(isExist)
+
+      const options={upsert:true}
+      const query={email:user?.email}
+      const updateDoc ={
+        $set:{
+          ...user,
+          Timestamp:Date.now()
+        }
+      }
+      const result  = await usersCollecction.updateOne(query,updateDoc,options)
+    res.send(result)
     })
 
 
@@ -50,6 +70,24 @@ async function run() {
       const result=campCollection.insertOne(campData)
       res.send(result)
     })
+
+    //get all camps for organizer(host)
+    app.get('/my-listing/:email',async (req,res)=>{
+      const email=req.params.email
+  let query={'organizer.email':email}
+      const result = await campCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // delelte a camp 
+    app.delete('/camp/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result=await campCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
 
 
         // Get a single camp  data from db using _id
