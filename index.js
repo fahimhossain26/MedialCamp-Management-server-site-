76dcd261f29996c8565b3ca15923b03231534560
fahimhossain26 <ismailhossainfahim64@gmail.com>
2024-06-07 15:08:44 +0600

    save a camp Data in Db

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ require('dotenv').config()
 const port=process.env.PORT ||5000;
 
 //midleware
-app.use(cors());
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+// app.use(cors());
 app.use(express.json());
 
 
