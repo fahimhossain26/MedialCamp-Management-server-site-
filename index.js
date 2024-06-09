35c813e42639d5beda0ef87f739475b4380f7831
fahimhossain26 <ismailhossainfahim64@gmail.com>
2024-06-09 23:16:54 +0600

    corsoption edit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express=require('express')
-const app= express();
-const cors=require('cors')
+const express = require('express')
+const app = express();
+const cors = require('cors')
 require('dotenv').config()
-const port=process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 
 //midleware
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: ['http://localhost:5173','https://assignment-12-4b1db.web.app', 'https://assignment-12-server-ruby.vercel.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
-// app.use(cors());
+
 app.use(express.json());
 
 
@@ -32,76 +32,95 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
-    await client.connect();
+
+    // await client.connect();
 
     const campCollection = client.db("MCMS").collection("camps");
-    const usersCollecction=client.db("MCMS").collection("users");
+    const usersCollecction = client.db("MCMS").collection("users");
 
     //get all camps
-    app.get('/camps',async (req,res)=>{
+    app.get('/camps', async (req, res) => {
       const result = await campCollection.find().toArray()
       res.send(result)
     })
 
     //save a user data in DB 
-    app.put('/user',async (req,res)=>{
-      const user=req.body
-//check if user already exist in Db 
-const isExist= await usersCollecction.findOne({email: user?.email})
-if(isExist) return res.send(isExist)
+    app.put('/user', async (req, res) => {
+      const user = req.body
+      const query = { email: user?.email }
+      //check if user already exist in Db 
+      const isExist = await usersCollecction.findOne(query)
+      // if(isExist) return res.send(isExist)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          const result = await usersCollecction.updateOne(query,
+            {
+              $set: { status: user?.status },
+            })
+            return res.send(result)
 
-      const options={upsert:true}
-      const query={email:user?.email}
-      const updateDoc ={
-        $set:{
-          ...user,
-          Timestamp:Date.now()
+        }
+        else {
+          return res.send(isExist)
         }
       }
-      const result  = await usersCollecction.updateOne(query,updateDoc,options)
-    res.send(result)
+//save user for first time 
+      const options = { upsert: true }
+
+      const updateDoc = {
+        $set: {
+          ...user,
+          Timestamp: Date.now()
+        }
+      }
+      const result = await usersCollecction.updateOne(query, updateDoc, options)
+      res.send(result)
     })
 
+    //get all users from Db
+    app.get('/users', async (req, res) => {
+      const result = await usersCollecction.find().toArray()
+      res.send(result)
+    })
 
     //save a camp Data in Db 
-    app.post('/camp',async (req,res)=>{
-      const campData=req.body 
-      const result=campCollection.insertOne(campData)
+    app.post('/camp', async (req, res) => {
+      const campData = req.body
+      const result = campCollection.insertOne(campData)
       res.send(result)
     })
 
     //get all camps for organizer(host)
-    app.get('/my-listing/:email',async (req,res)=>{
-      const email=req.params.email
-  let query={'organizer.email':email}
+    app.get('/my-listing/:email', async (req, res) => {
+      const email = req.params.email
+      let query = { 'organizer.email': email }
       const result = await campCollection.find(query).toArray()
       res.send(result)
     })
 
     // delelte a camp 
-    app.delete('/camp/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id: new ObjectId(id)}
-      const result=await campCollection.deleteOne(query)
+    app.delete('/camp/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await campCollection.deleteOne(query)
       res.send(result)
     })
 
 
 
 
-        // Get a single camp  data from db using _id
-        app.get('/camp/:id', async (req, res) => {
-          const id = req.params.id
-          const query = { _id: new ObjectId(id) }
-          const result = await campCollection.findOne(query)
-          res.send(result)
-        })
+    // Get a single camp  data from db using _id
+    app.get('/camp/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await campCollection.findOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -111,10 +130,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('server iis running ')
+app.get('/', (req, res) => {
+  res.send('server iis running ')
 })
 
-app.listen(port,()=>{
-    console.log(`MCMS is runningg onn the port ${port}`);
+app.listen(port, () => {
+  console.log(`MCMS is runningg onn the port ${port}`);
 })
